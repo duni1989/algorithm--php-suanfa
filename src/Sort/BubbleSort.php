1e088efbf22f2ddb72030437f61415c1e5a24afb
xuanxingang <1032047300@qq.com>
2020-02-08 19:45:24 +0800

    第二种方式

--- src/Sort/BubbleSort.php
+++ src/Sort/BubbleSort.php
@@ -17,7 +17,7 @@
  */
 
 // +--------------------------------------------------------------------------
-// | 解题方式    | 这儿，可能有用的解决方案
+// | 解题方式一：    | 这儿，可能有用的解决方案
 // +--------------------------------------------------------------------------
 
 /**
@@ -74,3 +74,148 @@ array(10) {
   int(423)
 }
  */
+
+
+
+
+
+
+
+
+// +--------------------------------------------------------------------------
+// | 解题方式 二：   | 这儿，可能有用的解决方案
+// +--------------------------------------------------------------------------
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 2020/2/6
+ * Time: 10:22 AM
+ */
+
+/*** 冒泡排序 ***/
+class BubbleSort
+{
+    /**
+     * 第一个版本(原始版本)：
+     */
+    public function sortV1($arr)
+    {
+        if (empty($arr)) {
+            return [];
+        }
+
+        if (!is_array($arr)) {
+            return [];
+        }
+        $tmp = 0;
+        for ($i = 0; $i < count($arr); $i++) {
+            for ($j = 0; $j < count($arr) - $i - 1; $j++) {
+                if ($arr[$j] > $arr[$j + 1]) {
+                    $tmp = $arr[$j];
+                    $arr[$j] = $arr[$j + 1];
+                    $arr[$j + 1] = $tmp;
+                }
+            }
+        }
+        return $arr;
+    }
+
+    /**
+     *第二个版本：如果我们能判断出数列已经全部是有序的，并且做出标记，剩下的几轮排序就可以不必执行，提早结束工作。
+     */
+    public function sortV2($arr)
+    {
+        if (empty($arr)) {
+            return [];
+        }
+        if (!is_array($arr)) {
+            return [];
+        }
+        $tmp = 0;
+        for ($i = 0; $i < count($arr); $i++) {
+            $isSorted = true;//有序标记，每一轮的初始值是true
+            for ($j = 0; $j < count($arr) - $i - 1; $j++) {
+                if ($arr[$j] > $arr[$j + 1]) {
+                    $tmp = $arr[$j];
+                    $arr[$j] = $arr[$j + 1];
+                    $arr[$j + 1] = $tmp;
+                    //有序元素交换,所以不是有序，标记变化为false
+                    $isSorted = false;
+                }
+            }
+            //  这一版代码做了小小的改动，利用布尔变量isSorted作为标记。
+            //  如果在本轮排序中，元素有交换，则说明数列无序；如果没有元素交换，说明数列已然有序，直接跳出大循环。
+            if ($isSorted) {
+                break;
+            }
+        }
+        return $arr;
+    }
+
+    /**
+     * 第三个版本：
+     */
+    public function sortV3(array $arr)
+    {
+        if (empty($arr)) {
+            return [];
+        }
+        if (!is_array($arr)) {
+            return [];
+        }
+        $tmp = 0;
+        $lastExchangeIndex = 0;//最后一次交换的位置
+        $sortBorder = count($arr) - 1;//无序数列的边界，每次比较只需要比较大道这里为止
+        for ($i = 0; $i < count($arr); $i++) {
+            $isSorted = true;
+            for ($j = 0; $j < $sortBorder; $j++) {
+                if ($arr[$j] > $arr[$j + 1]) {
+                    $tmp = $arr[$j];
+                    $arr[$j] = $arr[$j + 1];
+                    $arr[$j + 1] = $tmp;
+                    //有元素交换，所以不是有序，标记为false
+                    $isSorted = false;
+                    //把无序数列的边界更新为最后一次换元素的位置
+                    $lastExchangeIndex = $j;
+                }
+            }
+            $sortBorder = $lastExchangeIndex;
+            if ($isSorted) {
+                break;
+            }
+        }
+        return $arr;
+    }
+}
+
+
+$arr = [5, 8, 6, 3, 9, 2, 1, 7];//顺序全部无序
+
+$bubble = new BubbleSort();
+//$result = $bubble->sortV1($arr);
+//print_r($result);
+
+//$result = $bubble->sortV2($arr);
+//print_r($result);
+
+//这个数列的特点是前半部分（3，4，2，1）无序，后半部分（5，6，7，8）升序，并且后半部分的元素已经是数列最大值。
+$arr = [3, 4, 2, 1, 5, 6, 7, 8];
+$result = $bubble->sortV3($arr);
+print_r($result);
+
+
+
+Array
+(
+    [0] => 1
+    [1] => 2
+    [2] => 3
+    [3] => 4
+    [4] => 5
+    [5] => 6
+    [6] => 7
+    [7] => 8
+)
+
+
